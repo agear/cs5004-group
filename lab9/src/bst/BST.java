99e417b0e9594f3d30d8a9df9452303d5c120781
Alice <alice.tilles@gmail.com>
2019-03-18 18:51:54 -0400

    I pushed it first

--- lab9/src/bst/BST.java
+++ lab9/src/bst/BST.java
@@ -1,4 +1,36 @@
 package bst;
 
+/**
+ * This interface represents a Binary Search Tree of a generic object.
+ */
 public interface BST {
+
+  /** Adds a comparable object to the tree in its proper place.
+   *
+   * @param obj  The object to put in the tree
+   */
+  public void add(int obj);
+
+
+  /** Counts the number of objects in this data structre.
+   *
+   * @return the number of objects
+   */
+  public int getSize();
+
+
+  /** Iterates through the tree attempting to find the specified object.
+   *
+   * @param obj the object to find
+   * @return true if object is in the tree, false otherwise
+   */
+  public boolean present(int obj);
+
+
+  /** Returns the smallest object (defined by the ordering) in the tree,
+   * and null if the tree is empty.
+   *
+   * @return the smallest object or null
+   */
+  public int minimum();
 }
--- lab9/src/bst/BSTImpl.java
+++ lab9/src/bst/BSTImpl.java
@@ -1,4 +1,52 @@
 package bst;
 
+/**
+ * This class represents a binary search tree. It can add, it can get its size, it can detect
+ * if an inputted object is present, and it can find the minimum object.
+ */
 public class BSTImpl implements BST {
+
+  private
+
+
+  /**
+   * Adds a comparable object to the tree in its proper place.
+   *
+   * @param obj The object to put in the tree
+   */
+  @Override
+  public void add(int obj) {
+
+  }
+
+  /**
+   * Counts the number of objects in this data structre.
+   *
+   * @return the number of objects
+   */
+  @Override
+  public int getSize() {
+    return 0;
+  }
+
+  /**
+   * Iterates through the tree attempting to find the specified object.
+   *
+   * @param obj the object to find
+   * @return true if object is in the tree, false otherwise
+   */
+  @Override
+  public boolean present(int obj) {
+    return false;
+  }
+
+  /** Returns the smallest object (defined by the ordering) in the tree,
+   * and null if the tree is empty.
+   *
+   * @return the smallest object or null
+   */
+  @Override
+  public int minimum() {
+    return 0;
+  }
 }
