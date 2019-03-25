25625a16ebd9ba3dcd6edabe6e4c3694c3f9f060
Alex Gear <alexgear@gmail.com>
2019-03-25 10:49:58 -0400

    Added Javadoc to Filter.java

--- Assignment8/src/imageProcessing/Filter.java
+++ Assignment8/src/imageProcessing/Filter.java
@@ -1,24 +1,30 @@
 package imageProcessing;
 
 /**
- * TODO Javadoc.
+ * This class represents a filter to apply to an image. A filter has a "Kernel" which is a 2D array
+ * of numbers with odd dimensions (3x3, 5x5, etc).
  */
 public class Filter {
 
   double kernel[][];
 
 
+  // TODO Throw exception if kernel is not odd x odd
+
   /**
-   * TODO Javadoc.
-   * @param kernel
+   * Constructs a filter object. A filter has a "Kernel" which is a 2D array of doubles with odd
+   * dimensions (3x3, 5x5, etc).
+   *
+   * @param kernel A 2D array of numbers with odd dimensions (3x3, 5x5, etc).
    */
-  public Filter(double kernel[][]){
+  public Filter(double kernel[][]) {
     this.kernel = kernel;
   }
 
   /**
-   * TODO Javadoc.
-   * @return
+   * Returns a copy of the Kernel of this Filter.
+   *
+   * @return 2D array of doubles.
    */
   public double[][] getData() {
     return this.kernel.clone();
