7c1e6e79bdd1e2b31301b21a3d0312657a8dbfdb
Alex Gear <alexgear@gmail.com>
2019-03-27 12:03:17 -0400

    Completed some TODOs

--- lab10/src/GradeRecord.java
+++ lab10/src/GradeRecord.java
@@ -3,8 +3,8 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * Represents a student's transcript. A student can add a course, as long as it is
- * in the course directory (CREDITHOURS). They add a course with a letter grade.
+ * Represents a student's transcript. A student can add a course, as long as it is in the course
+ * directory (CREDITHOURS). They add a course with a letter grade.
  */
 public class GradeRecord implements Subject {
 
@@ -20,11 +20,9 @@ public class GradeRecord implements Subject {
   private final List<String> CORECOURSES;
 
 
-
   /**
-   * Initializes a grade record. It stores the default values for credit hours,
-   * for the relationship between a grade letter and a GPA value, and lists of the
-   * core MSCS and ALIGN program courses.
+   * Initializes a grade record. It stores the default values for credit hours, for the relationship
+   * between a grade letter and a GPA value, and lists of the core MSCS and ALIGN program courses.
    */
   public GradeRecord() {
 
@@ -80,23 +78,28 @@ public class GradeRecord implements Subject {
 
   }
 
-//TODO What happens if the object is already in the list?
   /**
-   * Signs up an observer object to be notified when the state of this GradeRecord changes.
+   * Subscribes an observer object to be notified when the state of this GradeRecord changes. Does
+   * nothing if the observer is already subscribed.
+   *
    * @param registree The object that wants to know what's going on.
    */
   public void register(Observer registree) {
-    this.observerList.add(registree);
+    if (!observerList.contains(registree)) {
+      this.observerList.add(registree);
+    }
   }
 
-  //TODO what happens if the object isn't in the list?
   /**
-   * Removes an observer from the observer list so they don't get notified about
-   * state changes to this grade record anymore.
+   * Removes an observer from the observer list so they don't get notified about state changes to
+   * this GradeRecord anymore. Does nothing if the observer is not in the observer list.
+   *
    * @param deregistree the object that will be removed from observer list.
    */
   public void remove(Observer deregistree) {
-    this.observerList.remove(deregistree);
+    if (observerList.contains(deregistree)) {
+      this.observerList.remove(deregistree);
+    }
   }
 
   /**
@@ -108,11 +111,12 @@ public class GradeRecord implements Subject {
     }
   }
 
-  /** //TODO What if the course has already been taken? Do we update the grade?
-   * Adds a course grade to this object, and recalculates GPA. Notifies observers that the state
-   * has changed.
+  /**
+   * //TODO What if the course has already been taken? Do we update the grade? Adds a course grade
+   * to this object, and recalculates GPA. Notifies observers that the state has changed.
+   *
    * @param course The course that has been taken.
-   * @param grade The grade earned in that course.
+   * @param grade  The grade earned in that course.
    * @throws IllegalArgumentException if the course doesn't exist at NEU.
    */
   public void addCourseGrade(String course, String grade) throws IllegalArgumentException {
@@ -132,21 +136,20 @@ public class GradeRecord implements Subject {
     this.studentGrades.put(course, grade);
 
     // Calculate and update GPA.
-    this.gpa = this.calculateGPA();
+    this.calculateGPA();
 
     //Calculate and update Core GPA.
-    this.coreGPA = this.calculateCoreGPA();
+    //this.coreGPA = this.calculateCoreGPA();
+    this.calculateCoreGPA();
 
     // Inform observers of state change.
     notifyObservers();
   }
 
-  //TODO return void instead?
-  /** Calculates the student's GPA based on their grade and the weight of the course.
-   *
-   * @return the student's GPA (in a numeric value).
+  /**
+   * Calculates the student's GPA based on their grade and the weight of the course.
    */
-  public double calculateGPA() {
+  public void calculateGPA() {
 
     // Accumulate a list of courses with grades
     List<String> eligibleCourses = new ArrayList<String>();
@@ -164,7 +167,8 @@ public class GradeRecord implements Subject {
       }
 
     }
-    //TODO Duplicate code to new function
+    //TODO Duplicate code to new function:
+
     // Initialize variable to store total GPA points earned
     double totalHours = 0;
     double qualityPoints = 0;
@@ -184,40 +188,37 @@ public class GradeRecord implements Subject {
 
     }
 
-    // Calculate GPA, save it, return it
-    double gpa = qualityPoints / totalHours;
+    // Update GPA and total hours.
+    this.gpa = qualityPoints / totalHours;
     this.totalHours = totalHours;
-    this.gpa = gpa;
-    return this.gpa; //TODO return gpa?
   }
 
-  /** The core courses for a regular MS student are: CS 5010, CS 5800 and one of CS 5500 and
-   * CS 5600. For ALIGN students, CS 5004 substitutes CS 5010 as a core course.
-   * These are the required courses for graduation with a GPA of 3.0+. This method
-   * calculates that GPA based on these courses.
-   *
-   * @return the GPA of these core courses
-   */ //TODO Void return?
-  public double calculateCoreGPA() {
+  /**
+   * The core courses for a regular MS student are: CS 5010, CS 5800 and one of CS 5500 and CS 5600.
+   * For ALIGN students, CS 5004 substitutes CS 5010 as a core course. These are the required
+   * courses for graduation with a GPA of 3.0+. This method calculates that GPA based on these
+   * courses.
+   */
+  public void calculateCoreGPA() {
 
     // Accumulate a list of courses with grades
     List<String> eligibleCourses = new ArrayList<String>();
 
     // For each course, maybe add it to the list of eligible courses
-    for (String course : this.courses ) {
+    for (String course : this.courses) {
 
       // Get the student's grade
       String thisGrade = studentGrades.get(course);
 
       // If the student has a valid grade, add the course to eligible list.
-      if ( GRADEVALUES.keySet().contains(thisGrade)
+      if (GRADEVALUES.keySet().contains(thisGrade)
               && (this.CORECOURSESALIGN.contains(course)
               || this.CORECOURSES.contains(course))) {
         eligibleCourses.add(course);
       }
 
     }
-  //TODO Duplicate code.
+    //TODO Duplicate code.
     // Initialize variable to store total GPA points earned
     double totalHours = 0;
     double totalQualityPoints = 0;
@@ -240,14 +241,13 @@ public class GradeRecord implements Subject {
     }
 
     // If the student has done 0 core course hours, then their core GPA is 0.
-    if (totalHours == 0 ) {
-      return 0.0;
+    if (totalHours == 0) {
+      //return 0.0;
+      this.coreGPA = 0.0;
     }
 
-    // Calculate core GPA, save it, return it
-    double coreGPA = totalQualityPoints / totalHours;
-    this.coreGPA = coreGPA;
-    return coreGPA;
+    // Calculate and update core GPA
+    this.coreGPA = totalQualityPoints / totalHours;
   }
 
   /**
@@ -258,9 +258,10 @@ public class GradeRecord implements Subject {
   public double getGPA() {
 
     // Make sure it is updated before returning the value.
-    this.gpa = this.calculateGPA();
-    //TODO Return duplicate.
-    return this.gpa;
+    this.calculateGPA();
+    // Return a copy of this.gpa.
+    double gpa = this.gpa;
+    return gpa;
   }
 
   /**
@@ -271,7 +272,8 @@ public class GradeRecord implements Subject {
   public double getCoreGPA() {
 
     // Make sure it is updated before returning the value.
-    this.coreGPA = this.calculateCoreGPA();
+    //this.coreGPA = this.calculateCoreGPA();
+    this.calculateCoreGPA();
     //TODO Return duplicate.
     return this.coreGPA;
   }
--- lab10/src/Subject.java
+++ lab10/src/Subject.java
@@ -1,26 +1,25 @@
-/** //TODO
- * Subject: This is the object that contains the data of interest to other objects.
+/**
+ * Interface for Objects that contain data of interest to other objects.
  */
 public interface Subject {
 
   /**
-   * This method is called in order to add an observer to be notified of changes
-   * by this subject object.
+   * This method is called in order to add an observer to be notified of changes by this subject
+   * object.
    */
   void register(Observer registree);
 
-//TODO Necessary?
   /**
-   * Removes an observer from the observer list so they don't get notified about
-   * changes/milestones to this grade record anymore.
-   * @param deregistree the object that will be removed from observer list
+   * Removes an observer from the observer list so they don't get notified about changes/milestones
+   * to this grade record anymore.
+   *
+   * @param deregistree the object that will be removed from observer list.
    */
   void remove(Observer deregistree);
 
-  //TODO
   /**
-   * Sends an update to all observers when something happens (e.g., a course is added
-   * to the grade record).
+   * Sends a notification to all observers when and object implementing this interfaces' state
+   * changes.
    */
   void notifyObservers();
 }
