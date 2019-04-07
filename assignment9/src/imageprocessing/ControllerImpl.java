23058fc5b5a63c80a7d6b777e7bc0c7f1f47f070
Alex Gear <alexgear@gmail.com>
2019-04-07 16:25:46 -0400

    Current working build

--- assignment9/src/imageprocessing/ControllerImpl.java
+++ assignment9/src/imageprocessing/ControllerImpl.java
@@ -1,4 +1,13 @@
 package imageprocessing;
 
+import java.util.Scanner;
+
 public class ControllerImpl implements IController {
+  public static void main(String[] args){
+    int num1, num2;
+    Scanner scan = new Scanner(System.in);
+    num1 = scan.nextInt();
+    num2 = scan.nextInt();
+    System.out.printf("%d", num1 + num2);
+  }
 }
--- assignment9/src/imageprocessing/ProgramRunner.java
+++ assignment9/src/imageprocessing/ProgramRunner.java
@@ -14,58 +14,70 @@ public class ProgramRunner {
   public static void main() throws IOException {
     IModel model = new ModelImpl();
 
-
-    //Import image to manipulate.
-    model.load("./res/shadowresize.jpg", "shadow");
-    model.load("./res/santaferesized.jpg", "santafe");
-
-    //Apply filters
-    model.applyBlur("shadow");
-    model.save("shadow-blur");
-
-    model.applySharpen("shadow");
-    model.save("shadow-sharp");
-
-    //Apply transformations
-    model.applySepia("shadow");
-    model.save("shadow-sepia");
-
-    model.applyGreyscale("shadow");
-    model.save("shadow-greyscale");
-
-    //Apply filters
-    model.applyBlur("santafe");
-    model.save("santafe-blur");
-
-    model.applySharpen("santafe");
-    model.save("santafe-sharp");
-
-    //Apply transformations
-    model.applySepia("santafe");
-    model.save("santafe-sepia");
-
-    model.applyGreyscale("santafe");
-    model.save("santafe-greyscale");
-
-    //Draw Flags
-    model.drawFlag(908,Country.GREECE);
+    model.drawFlag(500, Country.GREECE);
+    model.applyBlur("flag_1");
+    model.applyBlur("flag_1-blur");
+    model.applyBlur("flag_1-blur-blur");
+    model.applyBlur("flag_1-blur-blur-blur");
+    model.applyDither("flag_1-blur-blur-blur");
     model.save("flag_1");
+    model.save("flag_1-blur");
+    model.save("flag_1-blur-blur");
+    model.save("flag_1-blur-blur-blur");
+    model.save("flag_1-blur-blur-blur-dither");
 
-    model.drawFlag(777,Country.SWITZERLAND);
-    model.save("flag_2");
 
-    model.drawFlag(800, Country.FRANCE);
-    model.save("flag_3");
-
-    //Draw Rainbows
-    model.drawRainbow(300, 500, Orientation.VERTICAL);
-    model.save("rainbow_1");
-
-    model.drawRainbow(300,500,Orientation.HORIZONTAL);
-    model.save("rainbow_2");
-
-    //Draw
-    model.drawCheckerBoard(100);
-    model.save("checkerboard_1");
+//    //Import image to manipulate.
+//    model.load("./res/shadowresize.jpg", "shadow");
+//    model.load("./res/santaferesized.jpg", "santafe");
+//
+//    //Apply filters
+//    model.applyBlur("shadow");
+//    model.save("shadow-blur");
+//
+//    model.applySharpen("shadow");
+//    model.save("shadow-sharp");
+//
+//    //Apply transformations
+//    model.applySepia("shadow");
+//    model.save("shadow-sepia");
+//
+//    model.applyGreyscale("shadow");
+//    model.save("shadow-greyscale");
+//
+//    //Apply filters
+//    model.applyBlur("santafe");
+//    model.save("santafe-blur");
+//
+//    model.applySharpen("santafe");
+//    model.save("santafe-sharp");
+//
+//    //Apply transformations
+//    model.applySepia("santafe");
+//    model.save("santafe-sepia");
+//
+//    model.applyGreyscale("santafe");
+//    model.save("santafe-greyscale");
+//
+//    //Draw Flags
+//    model.drawFlag(908,Country.GREECE);
+//    model.save("flag_1");
+//
+//    model.drawFlag(777,Country.SWITZERLAND);
+//    model.save("flag_2");
+//
+//    model.drawFlag(800, Country.FRANCE);
+//    model.save("flag_3");
+//
+//    //Draw Rainbows
+//    model.drawRainbow(300, 500, Orientation.VERTICAL);
+//    model.save("rainbow_1");
+//
+//    model.drawRainbow(300,500,Orientation.HORIZONTAL);
+//    model.save("rainbow_2");
+//
+//    //Draw
+//    model.drawCheckerBoard(100);
+//    model.save("checkerboard_1");
   }
 }
