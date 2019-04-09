0d2b21b61c96d4deda39b73d7524711895108025
Alex Gear <alexgear@gmail.com>
2019-04-09 11:38:44 -0400

    Controller now uses a switch statement for parsing

--- assignment9/src/imageprocessing/controller/ControllerImpl.java
+++ assignment9/src/imageprocessing/controller/ControllerImpl.java
@@ -6,11 +6,12 @@ import java.util.Scanner;
 
 import imageprocessing.model.IModel;
 
+
 public class ControllerImpl implements IController {
   final Readable in;
-//  final Appendable out;
+  //  final Appendable out;
   IModel model;
- // IView view;
+  // IView view;
 
   public ControllerImpl(IModel model, Readable in) {
     this.in = in;
@@ -18,35 +19,47 @@ public class ControllerImpl implements IController {
     this.model = model;
   }
 
-  //TODO Change to switch statement
+  //TODO Add more functionality
   public void go() throws IOException {
     Objects.requireNonNull(model);
-    String command = "";
-    String filename = "";
-    String imagename = "";
+    String command;
+    String filename;
+    String imageName = "";
 
     Scanner scan = new Scanner(this.in);
-    while(scan.hasNext()) {
+    while (scan.hasNext()) {
       command = scan.next();
-      if(command.equals("load")) {
-        filename = scan.next();
-        imagename = scan.next();
-        this.model.load(filename, imagename);
-        System.out.println("Loading "+filename+" as "+imagename+"...");
+      switch (command) {
+        case "load":
+          filename = scan.next();
+          imageName = scan.next();
+          this.model.load(filename, imageName);
+          System.out.println("Loading " + filename + " as " + imageName + "...");
+          break;
+        case "dither":
+          this.model.applyDither(imageName);
+          System.out.println("Dithering " + imageName + "...");
+          break;
+        case "blur":
+          this.model.applyBlur(imageName);
+          System.out.println("Applying blur to " + imageName + "...");
+          break;
+        case "sepia":
+          this.model.applySepia(imageName);
+          System.out.println("Applying sepia transformation to " + imageName + "...");
+          break;
+        case "greyscale":
+          this.model.applyGreyscale(imageName);
+          System.out.println("Applying greyscale transformation to " + imageName + "...");
+          break;
+        case "save":
+          imageName = scan.next();
+          System.out.println("Saving " + imageName + " to " + imageName + ".png");
+          model.save(imageName);
+          break;
+        default:
+          System.out.println("Command " + command + " not found");
       }
-      else if(command.equals("dither")){ this.model.applyDither(imagename);
-        System.out.println("Dithering "+imagename+"...");}
-      else if(command.equals("blur")){ this.model.applyBlur(imagename);
-        System.out.println("Applying blur to "+imagename+"...");}
-      else if(command.equals("sepia")){ this.model.applySepia(imagename);
-        System.out.println("Applying sepia transformation to "+imagename+"...");}
-      else if(command.equals("greyscale")){ this.model.applyGreyscale(imagename);
-      System.out.println("Applying greyscale transformation to "+imagename+"...");}
-      else if(command.equals("save")){ imagename = scan.next();
-        System.out.println("Saving "+imagename+" to "+imagename+".png");
-      model.save(imagename);}
-      //else if (command.equals("exit")){break;}
-      else{System.out.println("Command not found");}
     }
   }
 }
